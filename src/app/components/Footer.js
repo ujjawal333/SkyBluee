a95026898745e987b22f8b45fdf6150469fb65c5
li4nee <nishantpokharel9@gmail.com>
2025-04-04 22:18:02 +0545

    fix : social media links added

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import {FiDribbble, FaBehance, FaLinkedin, FaFacebookF, FiInstagram, FaTwitter, FiMail} from '../assets/icons/vander'
+import { AtSign } from 'lucide-react';
 
 export default function Footer() {
 
@@ -23,9 +24,9 @@ export default function Footer() {
 
                                 <div className="md:col-span-9">
                                     <ul className="list-disc footer-list ltr:md:text-right rtl:md:text-left text-center">
-                                        <li className="inline-block"><Link href="/index" className="text-gray-300 hover:text-gray-400 duration-500 ease-in-out font-medium me-4">Home</Link></li>
+                                        <li className="inline-block"><Link href="/" className="text-gray-300 hover:text-gray-400 duration-500 ease-in-out font-medium me-4">Home</Link></li>
                                         <li className="inline-block mt-[10px] md:mt-0"><Link href="/services" className="text-gray-300 hover:text-gray-400 duration-500 ease-in-out font-medium me-4">How it works</Link></li>
-                                        <li className="inline-block mt-[10px] md:mt-0"><Link href="" className="text-gray-300 hover:text-gray-400 duration-500 ease-in-out font-medium me-4">Create a job</Link></li>
+                                        <li className="inline-block mt-[10px] md:mt-0"><Link href="/job-list-six" className="text-gray-300 hover:text-gray-400 duration-500 ease-in-out font-medium me-4">Job</Link></li>
                                         <li className="inline-block mt-[10px] md:mt-0"><Link href="/aboutus" className="text-gray-300 hover:text-gray-400 duration-500 ease-in-out font-medium me-4">About us</Link></li>
                                         <li className="inline-block mt-[10px] md:mt-0"><Link href="/contact" className="text-gray-300 hover:text-gray-400 duration-500 ease-in-out font-medium">Contact us</Link></li>
                                     </ul>
@@ -41,17 +42,21 @@ export default function Footer() {
                 <div className="container text-center">
                     <div className="grid md:grid-cols-2 items-center gap-6">
                         <div className="ltr:md:text-left rtl:md:text-right text-center">
-                            <p className="mb-0 text-gray-300 font-medium">©{(new Date().getFullYear())}{" "} Jobstack. Design & Develop with <i className="mdi mdi-heart text-red-600"></i> by <Link href="https://shreethemes.in/" target="_blank" className="text-reset">Shreethemes</Link>.</p>
+                            <p className="mb-0 text-gray-300 font-medium">©{(new Date().getFullYear())}{" "}Sky Blue Hr Solutions Pvt.Ltd</p>
                         </div>
 
                         <ul className="list-none ltr:md:text-right rtl:md:text-left text-center">
-                            <li className="inline"><Link href="https://dribbble.com/shreethemes" target="_blank" className="btn btn-icon btn-sm border-2 border-gray-800 dark:border-gray-700 rounded-md hover:border-emerald-600 dark:hover:border-emerald-600 hover:bg-emerald-600 dark:hover:bg-emerald-600 text-white"><FiDribbble/></Link></li>
-                            <li className="inline"><Link href="https://www.behance.net/shreethemes" target="_blank" className="btn btn-icon btn-sm border-2 border-gray-800 dark:border-gray-700 rounded-md hover:border-emerald-600 dark:hover:border-emerald-600 hover:bg-emerald-600 dark:hover:bg-emerald-600 text-white"><FaBehance/></Link></li>
-                            <li className="inline"><Link href="http://linkedin.com/company/shreethemes" target="_blank" className="btn btn-icon btn-sm border-2 border-gray-800 dark:border-gray-700 rounded-md hover:border-emerald-600 dark:hover:border-emerald-600 hover:bg-emerald-600 dark:hover:bg-emerald-600 text-white"><FaLinkedin width={16}/></Link></li>
-                            <li className="inline"><Link href="https://www.facebook.com/shreethemes" target="_blank" className="btn btn-icon btn-sm border-2 border-gray-800 dark:border-gray-700 rounded-md hover:border-emerald-600 dark:hover:border-emerald-600 hover:bg-emerald-600 dark:hover:bg-emerald-600 text-white"><FaFacebookF/></Link></li>
-                            <li className="inline"><Link href="https://www.instagram.com/shreethemes/" target="_blank" className="btn btn-icon btn-sm border-2 border-gray-800 dark:border-gray-700 rounded-md hover:border-emerald-600 dark:hover:border-emerald-600 hover:bg-emerald-600 dark:hover:bg-emerald-600 text-white"><FiInstagram/></Link></li>
-                            <li className="inline"><Link href="https://twitter.com/shreethemes" target="_blank" className="btn btn-icon btn-sm border-2 border-gray-800 dark:border-gray-700 rounded-md hover:border-emerald-600 dark:hover:border-emerald-600 hover:bg-emerald-600 dark:hover:bg-emerald-600 text-white"><FaTwitter/></Link></li>
-                            <li className="inline"><Link href="mailto:support@shreethemes.in" className="btn btn-icon btn-sm border-2 border-gray-800 dark:border-gray-700 rounded-md hover:border-emerald-600 dark:hover:border-emerald-600 hover:bg-emerald-600 dark:hover:bg-emerald-600 text-white"><FiMail/></Link></li>
+                        <li className="inline">
+                        <a 
+                            href="https://www.threads.net/@sky_blue_1664" 
+                            rel="noopener noreferrer"
+                            className="btn btn-icon btn-sm border-2 border-gray-800 dark:border-gray-700 rounded-md hover:border-emerald-600 dark:hover:border-emerald-600 hover:bg-emerald-600 dark:hover:bg-emerald-600 text-white inline-flex items-center justify-center p-2 mx-1"
+                        >
+                            <AtSign size={16} />
+                        </a>
+                        </li>
+                            <li className="inline"><Link href="https://www.facebook.com/share/1L36TXwmmH/?mibextid=wwXIfr" target="_blank" className="btn btn-icon btn-sm border-2 border-gray-800 dark:border-gray-700 rounded-md hover:border-emerald-600 dark:hover:border-emerald-600 hover:bg-emerald-600 dark:hover:bg-emerald-600 text-white"><FaFacebookF/></Link></li>
+                            <li className="inline"><Link href="https://www.instagram.com/sky_blue_1664?igsh=bnp2amJlNTljZzBh&utm_source=qr" target="_blank" className="btn btn-icon btn-sm border-2 border-gray-800 dark:border-gray-700 rounded-md hover:border-emerald-600 dark:hover:border-emerald-600 hover:bg-emerald-600 dark:hover:bg-emerald-600 text-white"><FiInstagram/></Link></li>
                         </ul>
                     </div>
                 </div>
